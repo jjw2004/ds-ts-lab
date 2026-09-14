2c9d56ff0339cfdc9da29eeccdb02be21e482b16
Joey <20105823@setu.ie>
2026-09-14 12:18:46 +0100

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -78,3 +78,15 @@ function findFriends(
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+function addInterest(friend: Friend, interest: string): string[] {
+  if (friend.interests === undefined) {
+    friend.interests = [];
+  }
+
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[1], 'Politics'));
+console.log(addInterest(friends[0], 'Politics'));
